47e6999e4650a872e5ef175a00e41326f7df1657
Evheniia Savchenko <80389291+YevgeniyaSavchenko@users.noreply.github.com>
2022-10-20 22:38:02 +0200

    add homework

---
+++ hw201022/script.js
@@ -0,0 +1,23 @@
+
+  //Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+
+
+let camelize = (str) => {
+    let arr =[];
+    let newStr = '';
+
+    arr = str.split('-');
+
+    for(let i=0;i<arr.length;i++){
+        arr[i] = arr[i].slice(0,1).toUpperCase()+arr[i].slice(1);
+    };
+
+    newStr = arr.join('');
+
+    return newStr;
+}
+
+let str1 = 'my-short-string';
+
+console.log(camelize(str1));
